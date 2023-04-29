75c86c92e8fdcc1474a4ccf2285935723f94389e
Rafael Ortiz <rortiz@inkremental.co>
2023-04-29 07:26:42 -0500

    Fix index js cors error

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:3000', 'https://www.thisismyapp.com'];
+const whitelist = ['http://localhost:8080', 'https://www.thisismyapp.com'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('You are NOT allowed'));
+      callback(new Error('no permitido'));
     }
   }
 }
